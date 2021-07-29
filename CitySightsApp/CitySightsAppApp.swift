a31a468eea58d012cd254c86f59c62334803e073
Braydon Whitfield <whitfield17@gmail.com>
2021-07-29 12:39:48 -0400

    Initial Commit

---
+++ CitySightsApp/CitySightsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsAppApp.swift
+//  CitySightsApp
+//
+//  Created by Braydon Whitfield on 2021-07-29.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
